1abaaf59760b909704c25faf3df35be7e8ac8add
Maria Mendonca <maria.mendonca156@gmail.com>
2023-09-24 13:25:51 +0530

    Added Login condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log('token', token);
+  }, []);
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
 }
 
-export default App
+export default App;
